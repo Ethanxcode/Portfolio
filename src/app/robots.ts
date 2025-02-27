14e302e4ecb8ce89b9c2f0b100ccf560c8e8b9aa
Ethan <trannguyendanhuy2904@gmail.com>
2025-02-27 15:20:04 +0700

    Add robots

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: [],
+        },
+        sitemap: 'https://ethantran.is-a.dev/sitemap.xml',
+    };
+}
