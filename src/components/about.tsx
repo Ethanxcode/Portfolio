97a112549493d40b34f90d4c9f838e9a084b7a32
Ethan <trannguyendanhuy2904@gmail.com>
2025-03-18 13:03:06 +0700

    OK

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -18,17 +18,23 @@ export default function About() {
                 }}
             >
                 <p className="text-lg leading-7 text-muted-foreground">
-                    🎓 I am <strong>Tran Nguyen Dang Huy</strong> from 📍 Ho Chi Minh, Vietnam.
-                    I have graduated with a degree in <strong>Information Technology</strong> and
-                    currently work as a <strong>Full Stack Developer</strong> with over <strong>1 year of experience</strong>.
-                    My passion lies in building high-quality, scalable software applications, mastering both frontend
-                    and backend development. As the saying goes,
+                    🎓 I am <strong>Tran Nguyen Dang Huy</strong> from 📍 Ho Chi
+                    Minh, Vietnam. I have graduated with a degree in{' '}
+                    <strong>Information Technology</strong> and currently work
+                    as a <strong>Full Stack Developer</strong> with over{' '}
+                    <strong>1 year of experience</strong>. My passion lies in
+                    building high-quality, scalable software applications,
+                    mastering both frontend and backend development. As the
+                    saying goes,
                     <em> "Once you go full stack, you never go back."</em>
                     <br />
                     <br />
-                    📚 Besides coding, I enjoy reading books, particularly in self-help and fiction.
-                    Some of my favorites include <strong>Kafka on the Shore</strong> by <em>Haruki Murakami</em>
-                    and <strong>The Courage to Be Disliked</strong> by <em>Ichiro Kishimi & Fumitake Koga</em>.
+                    📚 Besides coding, I enjoy reading books, particularly in
+                    self-help and fiction. Some of my favorites include{' '}
+                    <strong>Kafka on the Shore</strong> by{' '}
+                    <em>Haruki Murakami</em>
+                    and <strong>The Courage to Be Disliked</strong> by{' '}
+                    <em>Ichiro Kishimi & Fumitake Koga</em>.
                 </p>
             </Reveal>
         </section>
